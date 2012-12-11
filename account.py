8cd32e5c0fc9f42ee5936e33db9911d03f8ef767
TheLover_Z <sethverlo5@gmail.com>
2012-12-12 00:35:27 +0800

    signin post done

--- account.py
+++ account.py
@@ -8,11 +8,43 @@ import tornado.auth
 
 from stormwind.db import MemberDBMixin
 from stormwind.db.models import Member
+from stormwind.ext.renren import RenrenMixin
 from stormwind.base import BaseHandler
 
-class SigninHandler(BaseHandler):
+class SigninHandler(BaseHandler, MemberDBMixin, tornado.auth.GoogleMixin):
     def get(self):
         self.render("account.signin.html")
+    def post(self):
+        args = self.get_argument_list(["email", "username", "password"], \
+                                      [self._("Email"), self._("Username"), self._("Password")], \
+                                      None)
+        error = []
+        for name, translate, value in args:
+            if not value:
+                error.append(self._("%s is required." % translate))
+                continue
+            if len(value) < 4:
+                error.append(self._("%s is too short." % translate))
+                continue
+            if len(value) > 32:
+                error.append(self._("%s is too long." % translate))
+                continue
+            if name == "email" and not self.check_email(value):
+                error.append(self._("Email address is invaild."))
+                continue
+        username = args[1][2]
+        password = args[2][2]
+        member = self.select_member_by_username_lower(username)
+        if member.password == self.encrypt_password(password):
+            auth = self.create_auth(member.id)
+            self.set_secure_cookie("auth", auth.secret)
+            self.set_secure_cookie("uid", str(auth.member_id))
+            self.redirect("/")
+        else:
+            error.append(self._("username/password not match"))
+            if error:
+                self.render("account.signin.html", locals())
+                return
 
 class SigninGoogleHandler(BaseHandler, tornado.auth.GoogleMixin, MemberDBMixin):
     @tornado.web.asynchronous
@@ -94,9 +126,22 @@ class SignoutHandler(BaseHandler):
         self.clear_all_cookies()
         self.redirect("/")
 
+class SigninRenrenHandler(BaseHandler, RenrenMixin):
+    def _on_auth(self, user):
+        logging.info(user)
+        self.finish()
+    @tornado.web.asynchronous
+    def get(self):
+        callback_uri = self.settings['base_domain'] + '/signin/renren'
+        if self.get_argument("code", None):
+            self.get_authenticated_user(self.async_callback(self._on_auth), callback_uri)
+            return
+        self.authorize_redirect(callback_uri = callback_uri)
+
 route = [
     (r'/signin', SigninHandler), 
     (r'/signin/google', SigninGoogleHandler), 
+    (r'/signin/renren', SigninRenrenHandler), 
     (r'/signup', SignupHandler), 
     (r'/signup/google', SignupHandler), 
     (r'/signout', SignoutHandler), 
